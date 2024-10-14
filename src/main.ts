bc42e04eee53e1251191fbfd714c5a72b20f2e8a
gabrielalmir <gabr.almir@gmail.com>
2024-10-14 17:51:51 -0300

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express from "express";
 import { env } from "./config/env";
 import { Database } from "./db";
@@ -11,6 +12,7 @@ async function main() {
     const port = env.PORT;
 
     app.use(express.json());
+    app.use(cors());
     app.use('/', urlRoutes);
 
     app.listen(port, () => {
